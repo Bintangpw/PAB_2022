0a97ff1bef6e1581048e19e2522816fe4e70eddf
bintang <bntngputro@gmail.com>
2022-04-14 00:04:44 +0700

    praktikum random word, camera, geolokasi

--- Praktikum1/lib/main2.dart
+++ Praktikum1/lib/main2.dart
@@ -9,9 +9,11 @@ void main() {
   runApp(const MyApp());
 }
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -19,37 +21,50 @@ class MyApp extends StatelessWidget {
       home: RandomWords(),
     );
   }
+  // #enddocregion build
 }
+// #enddocregion MyApp
 
+// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
+
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
       ),
+      // #docregion itemBuilder
       body: ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
 
-          final index = i ~/ 2;
+          final index = i ~/ 2; /*3*/
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
+          // #docregion listTile
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
             ),
           );
+          // #enddocregion listTile
         },
       ),
+      // #enddocregion itemBuilder
     );
   }
+  // #enddocregion RWS-build
+  // #docregion RWS-var
 }
+// #enddocregion RWS-var
 
 class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
